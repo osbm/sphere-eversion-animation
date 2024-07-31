b71683f7bc915d06fd54fac169b2570f7617e913
josephmfaulkner <13591699+josephmfaulkner@users.noreply.github.com>
2024-07-30 20:13:39 -0700

    increase size of floor

--- src/js/scene/objects/Ground.js
+++ src/js/scene/objects/Ground.js
@@ -2,22 +2,37 @@ import * as THREE from 'three';
 
 export default class {
     constructor(scene){
+
         this.plane = new THREE.Mesh(
-            new THREE.PlaneGeometry( 16, 16 ),
+            new THREE.PlaneGeometry( 100, 100),
             new THREE.MeshPhongMaterial( { color: 0xa17e24, specular: 0x101010 } )
         );
         this.plane.rotation.x = - Math.PI / 2;
         this.plane.position.y = -0.15;
         this.plane.receiveShadow = true;
         scene.add( this.plane );
+
+
+        this.largePlane = new THREE.Mesh(
+            new THREE.PlaneGeometry( 10000, 10000),
+            new THREE.MeshPhongMaterial( { color: 0xa17e24, specular: 0x101010 } )
+        );
+        this.largePlane.rotation.x = - Math.PI / 2;
+        this.largePlane.position.y = -0.18;
+        this.largePlane.receiveShadow = false;
+        scene.add( this.largePlane );
+
+
+
     }
 
     changeColor(newColor){
-        this.plane.material.color = newColor;
         this.plane.material  = new THREE.MeshPhongMaterial({ color: newColor, specular: 0x101010 });
+        this.largePlane.material  = new THREE.MeshPhongMaterial({ color: newColor, specular: 0x101010 });
     }
 
     toggleShowGround(showGround){
         this.plane.visible = showGround;
+        this.largePlane.visible = showGround;
     }
 }
